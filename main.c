73e7be0edd1c4e018e952c384dc3247b1003ead3
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-09-27 21:37:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,10 @@ int main() {
     int massive[9] = {99, 88, 77, 66, 55, 44, 33, 22, 11};
     printf("Your massive: \n");
     for (int i = 0; i < m; i++) printf("%d ", massive[i]);
-    printf("\n\n");
-
-    int mat1[2][2] = { { 1, 1 },
-                       { 1, 4 } };
-    int mat2[2][2] = { { 1, 2 },
-                       { 4, 1 } };
+    printf("\n");
+    int mat1[2][2] = { { 1, 1 },{ 1, 4 } };
+    int mat2[2][2] = { { 1, 2 },{ 4, 1 } };
     int res[2][2];
-
     for(int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             res[i][j] = 0;
